7ae3a9973eff6520f757d47fdc997369fbbe3c4a
AmandaBlessing <amandablessing1431@gmali.com>
2024-03-05 12:51:42 +0200

    calculated the avarege marks for students

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Calculator {
+    Scanner scanner = new Scanner(System.in);
+
     public void studentMarks() {
-        
+        System.out.println("Enter your Marks for each subject: ");
+
+        System.out.println("Physics: ");
+        int physics = scanner.nextInt();
+
+        System.out.println("Chemistry: ");
+        int chemistry = scanner.nextInt();
+
+        System.out.println("Economics: ");
+        int economics = scanner.nextInt();
+
+        System.out.println("Business: ");
+        int business = scanner.nextInt();
+
+        System.out.println("Information Technology: ");
+        int it = scanner.nextInt();
+
+        // Close the scanner to prevent resource leak
+        scanner.close();
+
+        int totalMarks = physics + chemistry + economics + business + it;
+        int averagePercentage = totalMarks / 5;
+
+        System.out.println("Average percentage: " + averagePercentage);
     }
 }
