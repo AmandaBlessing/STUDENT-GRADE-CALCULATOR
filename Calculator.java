fbd1da384942f987baa0b3fb546682e22bb67b4f
AmandaBlessing <amandablessing1431@gmali.com>
2024-03-05 17:27:58 +0200

    added grades for students

--- Calculator.java
+++ Calculator.java
@@ -27,6 +27,20 @@ public class Calculator {
         int totalMarks = physics + chemistry + economics + business + it;
         int averagePercentage = totalMarks / 5;
 
+        char grade = ' ';
+        if (averagePercentage >= 80) {
+            grade = 'A';
+        } else if (averagePercentage >= 60) {
+            grade = 'B';
+        } else if (averagePercentage >= 50) {
+            grade = 'C';
+        } else if (averagePercentage >= 40) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
         System.out.println("Average percentage: " + averagePercentage);
+        System.out.println("Grade: " + grade);
     }
 }
